abccfd22641c12634979dae4d171e00cf33e8bd4
tro16 <tim.roethig@yahoo.de>
2024-05-12 21:34:20 +0200

    testing colab execution

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 import torch, transformers, pyreft
-import pandas as pd 
+import pandas as pd
 from colorama import init, Fore
 
 init()
@@ -21,19 +21,21 @@ tokenizer = transformers.AutoTokenizer.from_pretrained(
     padding_side="right",
     token=os.environ["hf_token"],
 )
-tokenizer.pad_token = tokenizer.unk_token 
+tokenizer.pad_token = tokenizer.unk_token
 
-def prompt_template(prompt): 
+
+def prompt_template(prompt):
     return f"""<s>[INST]<<sys>>You are a helpful assistant<</sys>>
         {prompt}
         [/INST]"""
 
+
 # Test case
-prompt = prompt_template("who is Nicholas Renotte?") 
-print(Fore.CYAN + prompt) 
+prompt = prompt_template("who is Nicholas Renotte?")
+print(Fore.CYAN + prompt)
 tokens = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
 response = model.generate(tokens, max_new_tokens=128)
-print(Fore.MAGENTA + tokenizer.decode(response[0])) 
+print(Fore.MAGENTA + tokenizer.decode(response[0]))
 
 # Get the reft model
 """
@@ -50,34 +52,34 @@ reft_config = pyreft.ReftConfig(
 """
 reft_config = pyreft.ReftConfig(
     representations={
-        "component": f"model.layers[9].output", # string access to the model component
+        "component": f"model.layers[9].output",  # string access to the model component
         "intervention": pyreft.ConsreftIntervention(
             embed_dim=model.config.hidden_size,
             low_rank_dimension=1
         )
     }
 )
- 
+
 reft_model = pyreft.get_reft_model(model, reft_config)
 reft_model.set_device('cuda')
 
 # GRAB Data
 df = pd.read_csv('./llm-censor/knowledgeoverride.csv')
-X = df['Prompt'].values 
-y = df['Response'].values 
+X = df['Prompt'].values
+y = df['Response'].values
 
 # Operate on last token 
 data_module = pyreft.make_last_position_supervised_data_module(
-    tokenizer, 
-    model, 
-    [prompt_template(x) for x in X], 
-    y 
+    tokenizer,
+    model,
+    [prompt_template(x) for x in X],
+    y
 )
 
 # Training arguments 
 training_arguments = transformers.TrainingArguments(
-    num_train_epochs=100, 
-    output_dir='./models', 
+    num_train_epochs=100,
+    output_dir='./models',
     per_device_train_batch_size=2,
     learning_rate=2e-3,
     report_to="none",
@@ -86,8 +88,8 @@ training_arguments = transformers.TrainingArguments(
 
 # Trainer for the reft model
 trainer = pyreft.ReftTrainerForCausalLM(
-    model=reft_model, 
-    tokenizer=tokenizer, 
+    model=reft_model,
+    tokenizer=tokenizer,
     args=training_arguments,
     **data_module
 )
@@ -96,7 +98,7 @@ trainer = pyreft.ReftTrainerForCausalLM(
 _ = trainer.train()
 
 # Test case
-prompt = prompt_template("What university did Nicholas Renotte study at?")
+prompt = prompt_template("who is Nicholas Renotte?")
 print(Fore.CYAN + prompt)
 tokens = tokenizer(prompt, return_tensors='pt').to('cuda')
 
@@ -105,11 +107,14 @@ base_unit_position = tokens['input_ids'].shape[-1] - 1
 _, response = reft_model.generate(
     tokens,
     unit_locations={'sources->base': (None, [[[base_unit_position]]])},
-    intervene_on_prompt=True
+    intervene_on_prompt=True,
+    max_new_tokens=128
 )
 
+print(Fore.LIGHTGREEN_EX + tokenizer.decode(response[0]))
+
 # Save the model
-reft_model.set_device('cpu') 
+reft_model.set_device('cpu')
 reft_model.save(
     save_directory='./trained_intervention'
 )
