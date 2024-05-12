3efaefbc4d5c3454f34ca952ba1ddbde609244df
tro16 <tim.roethig@yahoo.de>
2024-05-12 22:22:34 +0200

    testing censoring

--- score.py
+++ score.py
@@ -6,20 +6,21 @@ from train import prompt_template, setup_base_model
 
 model, tokenizer = setup_base_model()
 
-# Test case
-prompt = prompt_template("What university did Nicholas Renotte study at?")
-print(prompt)
-tokens = tokenizer(prompt, return_tensors='pt').to('cuda')
+if __name__ == "__main__":
+    # Test case
+    prompt = prompt_template("What university did Nicholas Renotte study at?")
+    print(prompt)
+    tokens = tokenizer(prompt, return_tensors='pt').to('cuda')
 
-# # Load the reft model 
-reft_model = pyreft.ReftModel.load('./trained_intervention', model)
-reft_model.set_device('cuda')
+    # # Load the reft model
+    reft_model = pyreft.ReftModel.load('./trained_intervention', model)
+    reft_model.set_device('cuda')
 
-# Generate a prediction
-base_unit_position = tokens['input_ids'].shape[-1] - 1
-_, response = reft_model.generate(
-    tokens,
-    unit_locations={'sources->base': (None, [[[base_unit_position]]])},
-    intervene_on_prompt=True
-)
-print(tokenizer.decode(response[0]))
+    # Generate a prediction
+    base_unit_position = tokens['input_ids'].shape[-1] - 1
+    _, response = reft_model.generate(
+        tokens,
+        unit_locations={'sources->base': (None, [[[base_unit_position]]])},
+        intervene_on_prompt=True
+    )
+    print(tokenizer.decode(response[0]))
--- train.py
+++ train.py
@@ -30,90 +30,90 @@ def setup_base_model():
     return model, tokenizer
 
 
-model, tokenizer = setup_base_model()
+if __name__ == "__main__":
+    model, tokenizer = setup_base_model()
 
-# Test case
-print("---Before Knowledge Override---")
-prompt = prompt_template("What is Deutsche Bank?")
-print("Prompt:")
-print(prompt)
+    # Test case
+    print("---Before Knowledge Override---")
+    prompt = prompt_template("What is Deutsche Bank?")
+    print("Prompt:")
+    print(prompt)
 
-tokens = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
-response = model.generate(tokens, max_new_tokens=128)
-print("Answer:")
-print(tokenizer.decode(response[0]))
+    tokens = tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+    response = model.generate(tokens, max_new_tokens=128)
+    print("Answer:")
+    print(tokenizer.decode(response[0]))
 
-# Get the reft model
-reft_config = pyreft.ReftConfig(
-    representations={
-        "component": f"model.layers[9].output",
-        "low_rank_dimension": 4,
-        "intervention": pyreft.LoreftIntervention(
-            embed_dim=model.config.hidden_size,
-            low_rank_dimension=4
-        ) 
-    }
-)
+    # Get the reft model
+    reft_config = pyreft.ReftConfig(
+        representations={
+            "component": f"model.layers[9].output",
+            "low_rank_dimension": 4,
+            "intervention": pyreft.LoreftIntervention(
+                embed_dim=model.config.hidden_size,
+                low_rank_dimension=4
+            )
+        }
+    )
 
-reft_model = pyreft.get_reft_model(model, reft_config)
-reft_model.set_device('cuda')
+    reft_model = pyreft.get_reft_model(model, reft_config)
+    reft_model.set_device('cuda')
 
-# GRAB Data
-df = pd.read_csv('./llm-censor/knowledgeoverride.csv')
-X = df['Prompt'].values
-y = df['Response'].values
+    # GRAB Data
+    df = pd.read_csv('./llm-censor/knowledgeoverride.csv')
+    X = df['Prompt'].values
+    y = df['Response'].values
 
-# Operate on last token 
-data_module = pyreft.make_last_position_supervised_data_module(
-    tokenizer,
-    model,
-    [prompt_template(x) for x in X],
-    y
-)
+    # Operate on last token
+    data_module = pyreft.make_last_position_supervised_data_module(
+        tokenizer,
+        model,
+        [prompt_template(x) for x in X],
+        y
+    )
 
-# Training arguments 
-training_arguments = transformers.TrainingArguments(
-    num_train_epochs=64,
-    output_dir='./models',
-    per_device_train_batch_size=2,
-    learning_rate=2e-3,
-    report_to="none",
-    logging_steps=16
-)
+    # Training arguments
+    training_arguments = transformers.TrainingArguments(
+        num_train_epochs=64,
+        output_dir='./models',
+        per_device_train_batch_size=2,
+        learning_rate=2e-3,
+        report_to="none",
+        logging_steps=16
+    )
 
-# Trainer for the reft model
-trainer = pyreft.ReftTrainerForCausalLM(
-    model=reft_model,
-    tokenizer=tokenizer,
-    args=training_arguments,
-    **data_module
-)
+    # Trainer for the reft model
+    trainer = pyreft.ReftTrainerForCausalLM(
+        model=reft_model,
+        tokenizer=tokenizer,
+        args=training_arguments,
+        **data_module
+    )
 
-# Train the model!!
-_ = trainer.train()
+    # Train the model!!
+    _ = trainer.train()
 
+    # Test case
+    print("---After Knowledge Override---")
+    prompt = prompt_template("What is Deutsche Bank?")
+    print("Prompt:")
+    print(prompt)
 
-# Test case
-print("---After Knowledge Override---")
-prompt = prompt_template("What is Deutsche Bank?")
-print("Prompt:")
-print(prompt)
+    tokens = tokenizer(prompt, return_tensors='pt').to('cuda')
 
-tokens = tokenizer(prompt, return_tensors='pt').to('cuda')
+    # Generate a pyreft prediction
+    base_unit_position = tokens['input_ids'].shape[-1] - 1
+    _, response = reft_model.generate(
+        tokens,
+        unit_locations={'sources->base': (None, [[[base_unit_position]]])},
+        intervene_on_prompt=True,
+        max_new_tokens=128
+    )
+    print("Answer:")
+    print(tokenizer.decode(response[0]))
 
-# Generate a pyreft prediction
-base_unit_position = tokens['input_ids'].shape[-1] - 1
-_, response = reft_model.generate(
-    tokens,
-    unit_locations={'sources->base': (None, [[[base_unit_position]]])},
-    intervene_on_prompt=True,
-    max_new_tokens=128
-)
-print("Answer:")
-print(tokenizer.decode(response[0]))
-
-# Save the model
-reft_model.set_device('cpu')
-reft_model.save(
-    save_directory='./trained_intervention'
-)
+    # Save the model
+    reft_model.set_device('cpu')
+    reft_model.save(
+        save_directory='./trained_intervention'
+    )
