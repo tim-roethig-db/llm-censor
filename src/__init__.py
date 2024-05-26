2afbd58d2f90fe997501018e924727edef50446c
tro16 <tim.roethig@yahoo.de>
2024-05-26 15:40:10 +0200

    testing censoring

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,3 @@
 Module for transcribing speeches of 1 or multiple speakers
 """
 from .train import train
-from .speech2text import speech2text
-from .tools import get_secret
-from .main import transcribe
-from .post_processing import merge_rows_consecutive_speaker, format_time_stamp
